4fb84bd797c08daf301b58b45b1d573a56a4ee71
Philippe Gouin <bering@ringlogic.com>
2020-07-12 09:24:30 -0400

    Added NerdFonts icons

--- hello_user.sh
+++ hello_user.sh
@@ -10,26 +10,26 @@ if test -x $(which linux_logo); then linux_logo -l; fi
 echo
 
 (
-    echo -n "Uptime: "
+    echo -n "Uptime:ﮣ:"
     uptime -p
 
-    echo -n "Load averages: "
+    echo -n "Load averages::"
     cat /proc/loadavg
 
-    echo -n "Free memory: "
+    echo -n "Free memory::"
     free -h | head -n 2 | tail -n 1 | awk '{ print $7 }'
 
-    echo -n "Free swap: "
+    echo -n "Free swap:易:"
     free -h | tail -n 1 | awk '{ print $4}'
 
-    echo -n "Free space on /: "
+    echo -n "Free space on /::"
     df -h / | tail -n 1 | awk '{ print $4 }'
 
     while test -n "$1"; do
-        echo -n "Free space on $1: "
+        echo -n "Free space on $1::"
         df -h $1 | tail -n 1 | awk '{ print $4 }'
         shift
     done
 
     echo
-) | column -t -s ":" -o " :"
+) | column -t -s ":"
